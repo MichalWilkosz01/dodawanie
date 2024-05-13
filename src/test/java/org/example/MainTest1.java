3ed5e3f4690b5b9b60e983144a59fa55edb548ff
Michał Wilkosz <92877972+MichalWilkosz01@users.noreply.github.com>
2024-05-13 14:49:57 +0200

    update

--- src/test/java/org/example/MainTest1.java
+++ src/test/java/org/example/MainTest1.java
@@ -2,7 +2,7 @@ package org.example;
 
 import junit.framework.TestCase;
 
-public class MainTest1 extends TestCase {
+public class MainTest extends TestCase {
 
     public void testAdded() {
         Main main = new Main();
